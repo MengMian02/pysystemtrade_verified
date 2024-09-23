480a1bf4488c500ad61083aeab651fe97a1008ae
wanli15nian <jiang_jiahong@qq.com>
2024-09-23 19:37:12 +0800

    Update submission.py

--- refactory/submission.py
+++ refactory/submission.py
@@ -650,8 +650,9 @@ def caculate_instrument_weights(daily_ret):
     return weight
 
 
-def iter_method(curves, instrument):
-    curve = curves[instrument]
+def calculate_instrument_pnl(instrument):
+    pandl_calculator = pandl_calculator_for_subsystem_with_cash_costs(instrument)
+    curve = accountCurve(pandl_calculator)
     calculator = curve.pandl_calculator_with_costs
     pnl = calculator.as_pd_series(percent=False, curve_type=GROSS_CURVE)
     pnl.index = pd.to_datetime(pnl.index)
@@ -662,24 +663,16 @@ def iter_method(curves, instrument):
 def get_instrument_weight(my_config):
     instruments = my_config.instruments
 
-    dict_of_account_curves = {}
-    for instrument in instruments:
-        pandl_calculator = pandl_calculator_for_subsystem_with_cash_costs(instrument)
-        dict_of_account_curves[instrument] = accountCurve(pandl_calculator)
-    dict_of_account_curves = dictOfAccountCurves(dict_of_account_curves)
+    instrument_pnl = [calculate_instrument_pnl(instrument) for instrument in instruments]
+    df_pnl = pd.concat(instrument_pnl, axis=1)
+    df_pnl.columns = instruments
 
-    data_as_list = [iter_method(dict_of_account_curves, instrument) for instrument in instruments]
-
-    data_as_pd = pd.concat(data_as_list, axis=1)
-    data_as_pd.columns = instruments
-    curves = data_as_pd.resample("1B").sum()
+    curves = df_pnl.resample("1B").sum()
     curves[curves == 0.0] = np.nan
-
     weight = caculate_instrument_weights(curves)
-
     print(weight)
 
-    return weight
+    return
 
 
 if __name__ == '__main__':
